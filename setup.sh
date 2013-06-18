7f6f78de897876c264702de99ab380ba597c4acf
nobuyasu tomura <nbyst@lightning.lightning.localdomain>
2013-06-18 19:32:56 +0900

    setup.shでsymlink元のパスを$dotfilesと表現

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-cd 
-ln -s ~/.dotfiles/.tmux.conf .tmux.conf
-ln -s ~/.dotfiles/.zshrc .zshrc
-ln -s ~/.dotfiles/.gitconfig .gitconfig
-ln -s ~/.dotfiles/.aliasrc .aliasrc
+set -x
+currentdir=$(cd `dirname $0`; pwd)
+dotfilespath=~/.dotfiles
+
+ln -s $currentdir $dotfilespath
+ln -s $dotfilespath/.tmux.conf .tmux.conf
+ln -s $dotfilespath/.zshrc .zshrc
+ln -s $dotfilespath/.gitconfig .gitconfig
+ln -s $dotfilespath/.aliasrc .aliasrc
 
 #x
 #ln -s $HOME/dotfiles/x/.gtkrc-2.0 $HOME/.gtkrc-2.0
