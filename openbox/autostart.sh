bde0a53cb45f461bd85ff3d93ece46a8cfad2163
nbyst <wargs.no2tm@gmail.com>
2013-06-29 15:17:11 +0900

    openbox configuration

---
+++ openbox/autostart.sh
@@ -0,0 +1,2 @@
+tint2 -c ~/.config/tint2/v-white
+feh --bg-scale ~/.wallpaper
--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 cd 
-ln -s dotfiles/.tmux.conf .tmux.conf
-ln -s dotfiles/.zshrc .zshrc
-ln -s dotfiles/.gitconfig .gitconfig
-ln -s dotfiles/.aliasrc .aliasrc
+ln -s ~/.dotfiles/.tmux.conf .tmux.conf
+ln -s ~/.dotfiles/.zshrc .zshrc
+ln -s ~/.dotfiles/.gitconfig .gitconfig
+ln -s ~/.dotfiles/.aliasrc .aliasrc
 
 #x
 #ln -s $HOME/dotfiles/x/.gtkrc-2.0 $HOME/.gtkrc-2.0
