8a7007c3e283423b3b62f27f9a5374adb4b6c4f6
nbyst <wargs.no2tm@gmail.com>
2013-07-11 01:25:39 +0900

    syndaemon の-tオプションを削除

--- x202e-touchpad.sh
+++ x202e-touchpad.sh
@@ -2,7 +2,4 @@
 xinput --set-prop "ETPS/2 Elantech Touchpad" "Synaptics Tap Action"  2, 3, 0, 0, 1, 3, 0
 xinput --set-prop "ETPS/2 Elantech Touchpad" "Synaptics Click Action" 1, 3, 2
 
-#タッチパッドを無効化
-#shft,ctrl,altでは無効化させない(-K)
-#停止時間を1sに(-i 1)
-syndaemon -d -t -i 1 -K
+syndaemon -d -i 1 -K
