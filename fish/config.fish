00cd1fdff3eea9344c378a8139993a01b0537bc2
Nobuyasu Tomura <ntomura92@gmail.com>
2017-09-02 19:10:11 +0900

    fishの設定

--- fish/config.fish
+++ fish/config.fish
@@ -8,18 +8,32 @@ set --export PATH $PATH /opt/yarn-global/bin
 set --export PATH $PATH $HOME/.anyenv/bin
 anyenv init - fish | source
 
-# java (jenv tukaitai)
-set --export PATH $PATH  /opt/j/jdk1.8.0_131/bin
-set --export JAVA_HOME /opt/j/jdk1.8.0_131
+# java
+set --export PATH $PATH  /opt/java/java-home-symlink/bin
+set --export JAVA_HOME /opt/java/java-home-symlink
 
+# go
+set --export GOPATH ~/dev
 
-# === alias ===
+# === alias:linux ===
 alias g "git"
-alias p "sudo pacman"
 alias wh "which"
 alias fi "file"
 alias s "sudo"
 
-alias fr "fisher"
-alias yao "yaourt"
+# === alias:xorg ===
 alias pbcopy "xsel --clipboard --input"
+
+# === alias:fish ===
+alias fsr "fisher"
+alias vi-fishrc "vim ~/.config/fish/config.fish"
+
+# === alias:archlinux ===
+alias p "sudo pacman"
+alias yao "yaourt"
+
+# === alias:rails ===
+alias be "bundle exec"
+
+# === alias:js ===
+alias yn "yarn"
