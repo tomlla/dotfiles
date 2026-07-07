e29238faceacc85a061c2df36a958778cd82af71
Nobuyasu Tomura <ntomura92@gmail.com>
2026-07-07 16:09:23 +0900

    Ctrl - t で task fzf 時に task name 部分の split できていなかった

--- shell.d/task-fzf.bash
+++ shell.d/task-fzf.bash
@@ -1,12 +1,12 @@
 # Use Ctrl + t, to run
 _task-fzf() {
-  local task_line task_name
-  task_line=$(
+  local task_line=$(
       task --list-all --json \
-      | jq -r '.tasks[] | "\(.name) \(.desc)"' \
+      | jq -r '.tasks[] | "\(.name) \t # \(.desc)"' \
       | fzf
   )
   [ -z "$task_line" ] && return
+  task_name=$(echo $task_line | awk '{print $1}')
   echo task_name=$task_name
   READLINE_LINE="task $task_name"
   READLINE_POINT=${#READLINE_LINE}
